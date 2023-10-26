a3f1f75a9beb6b54e0e97d1d1e88d816cc9de89e
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-10-26 16:04:01 +0200

    feat(homepage) : Mise en place de la page d'accueil

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Bienvenue dans Git training</h1>
+    </div>
+    <div>
+       <p>Ce projet à pour but de référencer les bases de git pour être en mesure d'utiliser et comprendre la gestion de versions.</p> 
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
