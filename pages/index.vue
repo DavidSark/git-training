704473c93f80c0422ef7eba061b3584e55caf133
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-11-24 10:47:06 +0100

    feature(composant) : un composant nécessaire

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,8 @@
     </div>
     <div>
        <p>Ce projet à pour but de référencer les bases de git pour être en mesure d'utiliser et comprendre la gestion de versions.</p> 
+
+       <p>truc dont tu as besoin</p>
     </div>
 </template>
 
